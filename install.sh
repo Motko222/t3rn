233f5cc8f245df3fc7dac8125257110bb134587e
Motko <77466238+Motko222@users.noreply.github.com>
2025-03-25 23:36:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
 folder=$(echo $path | awk -F/ '{print $NF}')
 source /$path/config 
+tag=v0.59.0
 
 cd /root
 mkdir t3rn
 cd t3rn
-wget https://github.com/t3rn/executor-release/releases/download/$TAG/executor-linux-$TAG.tar.gz
-tar -xzf executor-linux-$TAG.tar.gz
+wget https://github.com/t3rn/executor-release/releases/download/$tag/executor-linux-$tag.tar.gz
+tar -xzf executor-linux-$tag.tar.gz
 
 #create service
 printf "[Unit]
