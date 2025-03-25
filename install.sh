9b0906a52a723748e5e5884ab70c5446dcb9296a
Motko <77466238+Motko222@users.noreply.github.com>
2025-03-25 23:35:09 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,15 +15,7 @@ After=network.target
 Wants=network-online.target
 
 [Service]
-Environment=\"ENVIRONMENT=testnet\"
-Environment=\"LOG_LEVEL=debug\"
-Environment=\"LOG_PRETTY=false\"
-Environment=\"EXECUTOR_PROCESS_BIDS_ENABLED=true\"
-Environment=\"EXECUTOR_PROCESS_ORDERS_ENABLED=true\"
-Environment=\"EXECUTOR_PROCESS_CLAIMS_ENABLED=true\"
-Environment=\"EXECUTOR_MAX_L3_GAS_PRICE=30000\"
-Environment=\"PRIVATE_KEY_LOCAL=$PK\"
-Environment=\"ENABLED_NETWORKS='arbitrum-sepolia,base-sepolia,optimism-sepolia,l2rn'\"
+EnvironmentFile=/root/scripts/$folder/config
 User=root
 Group=root
 ExecStart=/root/t3rn/executor/executor/bin/executor
