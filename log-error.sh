083cf7583043ee7b23b4569746a25146ee6f2372
Motko <77466238+Motko222@users.noreply.github.com>
2025-04-09 10:31:36 +0200

    Update log-error.sh

--- log-error.sh
+++ log-error.sh
@@ -3,4 +3,6 @@
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 
-journalctl -u $folder.service --since "1 hour ago" --no-hostname -o cat | grep -E "rror|ERR"
+read -p "Grep? " search
+
+journalctl -u $folder.service --since "1 day ago" --no-hostname -o cat | grep $search
