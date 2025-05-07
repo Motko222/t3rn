09c0d7430065b396484add97072f82d1f303f02a
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-07 17:22:58 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -10,12 +10,13 @@ version=$(cat /root/logs/t3rn-version)
 service=$(sudo systemctl status $folder --no-pager | grep "active (running)" | wc -l)
 errors=$(journalctl -u $folder.service --since "1 hour ago" --no-hostname -o cat | grep -c -E "level\":\"error")
 tx=$(journalctl -u $folder.service --since "1 hour ago" --no-hostname -o cat | grep -c -E "Tx batch item successful|Tx single item successful")
+fail=$(journalctl -u $folder.service --since "1 hour ago" --no-hostname -o cat | grep -c -E "Execution failed|Transmitter failed to submit tx")
 funds=$(journalctl -u t3rn-3.service --since "1 hour ago" --no-hostname -o cat | grep -E "INSUFFICIENT_FUNDS" | tail -1 | jq -r .networkId)
 balance=$(curl -sX 'GET' 'https://b2n.explorer.caldera.xyz/api/v2/addresses/'$WALLET -H 'accept: application/json' | jq -r .coin_balance | awk '{ printf ("%.0f\n",$1/1000000000000000000) }')
 
-status="ok" && message="tx=$tx bal=$balance"
-[ $errors -gt 200 ] && status="warning" && message="tx=$tx bal=$balance errors=$errors";
-[ ! -z $funds ] && status="warning" && message="tx=$tx bal=$balance funds=$funds";
+status="ok" && message="tx=$tx fail=$fail bal=$balance"
+[ $errors -gt 200 ] && status="warning" && message="tx=$tx fail=$fail bal=$balance errors=$errors";
+[ ! -z $funds ] && status="warning" && message="tx=$tx fail=$fail bal=$balance funds=$funds";
 [ $service -ne 1 ] && status="error" && message="service not running";
 
 cat >$json << EOF
